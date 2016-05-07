a24c24a7b1bd831561fb9a4087c589f4035690e8
Zlatko Hristov <zlatko.2create@gmail.com>
2016-05-07 13:29:18 +0300

    Remove extended orm.em.options

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -16,27 +16,21 @@ class MediaServiceProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
 
-        $app['orm.em.options'] = array_merge_recursive($app['orm.em.options'], array(
-            'mappings' => array(array(
-                    'type' => 'annotation',
-                    'namespace' => 'Media\Entity',
-                    'path' => __DIR__ . DIRECTORY_SEPARATOR . 'Entity'
-                ))
-        ));
-
-        $app['twig.path'] = array_merge_recursive($app['twig.path'], array(
-            __DIR__ . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'views'
-        ));
-        $app['twig'] = $app->share($app->extend('twig', function(\Twig_Environment $twig) {
-                    $twig->addExtension(new Twig\MediaExtension());
-                    return $twig;
-                }));
-        $app['twig.form.templates'] = array_merge_recursive($app['twig.form.templates'], array("media.type.html.twig"));
-
         $app['form.types'] = $app->share($app->extend('form.types', function ($types) {
                     $types[] = new Form\Type\MediaType();
                     return $types;
                 }));
+
+        $app['twig'] = $app->share($app->extend('twig', function(\Twig_Environment $twig) {
+                    $twig->addExtension(new Twig\MediaExtension());
+                    return $twig;
+                }));
+
+        $app['twig.path'] = array_merge_recursive($app['twig.path'], array(
+            __DIR__ . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'views'
+        ));
+
+        $app['twig.form.templates'] = array_merge_recursive($app['twig.form.templates'], array("media.type.html.twig"));
     }
 
 }
