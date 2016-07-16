3f24b40df5cee14bb65a53bcbf9984b2abd37856
Zlatko Hristov <zlatko.2create@gmail.com>
2016-07-16 13:46:40 +0300

    Fix silex2  compability

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -24,17 +24,17 @@ class MediaServiceProvider implements ServiceProviderInterface, BootableProvider
     {
 
         if ($app->offsetExists('form.factory')) {
-            $app['form.types'] = $app->share($app->extend('form.types', function ($types) use ($app) {
+            $app['form.types'] = $app->extend('form.types', function ($types) use ($app) {
                         $types[] = new Form\Type\MediaType($app);
                         return $types;
-                    }));
+                    });
         }
 
         if ($app->offsetExists('twig')) {
-            $app['twig'] = $app->share($app->extend('twig', function(\Twig_Environment $twig) use ($app) {
+            $app['twig'] = $app->extend('twig', function(\Twig_Environment $twig) use ($app) {
                         $twig->addExtension(new Twig\MediaExtension($app));
                         return $twig;
-                    }));
+                    });
 
             $app['twig.path'] = array_merge($app['twig.path'], array(
                 __DIR__ . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'views'
